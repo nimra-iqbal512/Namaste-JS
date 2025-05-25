66c291be57aac125d40ce7f0c1395b6244b472f7
nimra-iqbal512 <nimraiqbal512@gmail.com>
2025-05-25 16:43:02 +0500

    V2_Hoisting_MemoryAllocationAndCodeExecution

--- V2_Hoisting/script.js
+++ V2_Hoisting/script.js
@@ -1,7 +1,55 @@
+// ===========
+console.log(x); // undefined
+getNmae();      // Nimra
 var x = 7;
 function getNmae() {
-    console.log('nIMRA');
+    console.log('Nimra');
 }
 
-console.log(x);
-getNmae();
+// // ===========
+// console.log(x); // Error: x is not defined
+// getNmae();
+// function getNmae() {
+//     console.log('Nimra');
+// }
+
+// // ===========
+// function getNmae() {
+//     console.log('Nimra');
+// }
+// console.log(getNmae);
+
+// // ===========
+// console.log(getNmae);
+// function getNmae() {
+//     console.log('Nimra');
+// }
+
+
+// // // =========== Arrow functions--> also behave like variables(contains undefined)
+// console.log(getName); //undefined
+
+// var getName = () => {
+//     console.log('Nimra');
+// }
+
+
+// // // ===========
+// console.log(getName); //undefined
+// console.log(getName2); //function code
+
+// var getName = () => {
+//     console.log('Nimra');
+// }
+
+// function getName2(){
+//     console.log('Nimra');
+// }
+
+
+// // // =========== (Also behave slike a variable)
+// console.log(getName); //undefined
+// var getName = function(){
+//     console.log('Nimra');
+// }
+// console.log(getName); 
